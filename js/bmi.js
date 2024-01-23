83673391ffa14ada06d069dfd8450e9f276cbc30
johnkao <liberbay105@gmail.com>
2024-01-23 21:06:06 +0900

    外置.js並連動button

---
+++ js/bmi.js
@@ -0,0 +1,20 @@
+const userNameE1 = document.querySelector("#username");
+const heightE1 = document.querySelector("#userheight");
+const weightE1 = document.querySelector("#userwe");
+
+
+function goBmi() {
+    let height = heightE1.value;
+    let weight = weightE1.value;
+    let bmi = getBmi(height, weight);
+    console.log(bmi);
+
+}
+
+function getBmi(height, weight) {
+    let bmi = (weight / (height / 100) ** 2).toFixed(2);
+    if (isNaN(bmi)) {
+        return "數值錯誤!";
+    }
+    return bmi;
+}
