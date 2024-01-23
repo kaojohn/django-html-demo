f0f104b363f3741c1dfa12da6bc8b20b057aaef2
johnkao <liberbay105@gmail.com>
2024-01-23 22:16:20 +0900

    完成bmi邏輯跟顯示評語

--- js/bmi.js
+++ js/bmi.js
@@ -1,6 +1,8 @@
 const userNameE1 = document.querySelector("#username");
 const heightE1 = document.querySelector("#userheight");
 const weightE1 = document.querySelector("#userwe");
+const bmiE1 = document.querySelector(".seebmi");
+const commentE1 = document.querySelector(".comment");
 
 
 function goBmi() {
@@ -13,7 +15,17 @@ function goBmi() {
     }
 
     let bmi = getBmi(height, weight);
-    console.log(bmi);
+    bmiE1.innerText = bmi;
+
+    let comment;
+    if (bmi <= 18.5) {
+        comment = "體重過輕";
+    } else if (bmi < 24) {
+        comment = "健康體位♪";
+    } else {
+        comment = "體位異常( ﾟДﾟ)";
+    }
+    commentE1.innerText = comment;
 
 }
 
